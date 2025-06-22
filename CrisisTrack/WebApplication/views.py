da33b1d9916b05ec396719a7cb3e2301c6281880
Mihai-Alin Tufescu <92632143+mihaitufescu@users.noreply.github.com>
2025-06-22 16:12:28 +0300

    Update views.py

--- CrisisTrack/WebApplication/views.py
+++ CrisisTrack/WebApplication/views.py
@@ -62,14 +62,14 @@ def create_incident(request):
         form = IncidentCreationForm(request.POST)
         if form.is_valid():
             incident = form.save(commit=False)
-            incident.reported_by = request.user  # Attach the logged-in user to the incident
+            incident.reported_by = request.user
             incident.save()
 
-            # Set success message
+
             messages.success(request, "Incidentul a fost creat cu succes!")
 
-            # Redirect to the same page after the form submission
-            return HttpResponseRedirect(request.path)  # Redirect to the same page
+
+            return HttpResponseRedirect(request.path)  
 
     else:
         form = IncidentCreationForm()
@@ -79,9 +79,9 @@ def create_incident(request):
 @login_required
 def incident_list(request):
     if not request.user.is_superuser:
-        # Add an access denied message
+
         messages.error(request, "Accesul este restricționat. Doar administratorii pot vizualiza această pagină.")
-        # Redirect to the index page
+
         return redirect('index')
     incidents = Incident.objects.all()
     categories = IncidentCategory.objects.all()
@@ -107,15 +107,15 @@ def guideline_detail(request, incident_type):
     })
 
 def review_incident(request, incident_id):
-    # Fetch the incident by ID
+
     incident = get_object_or_404(Incident, id=incident_id)
     
-    # Handle GET request to show the form
+
     if request.method == 'GET':
         form = ReviewIncidentForm()
         return render(request, 'review_incident.html', {'incident': incident, 'form': form})
     
-    # Handle POST request to process the form and send the email
+
     elif request.method == 'POST':
         form = ReviewIncidentForm(request.POST)
         if form.is_valid():
@@ -123,13 +123,13 @@ def review_incident(request, incident_id):
             
             # Send email to the reported_by user
             if incident.reported_by.email:
-                from_email = os.getenv('SMTP_HOST')  # Assuming SMTP_HOST is your email environment variable
+                from_email = os.getenv('SMTP_HOST')  
                 
                 try:
                     send_mail(
                         subject=f"Incident Update: {incident.title}",
                         message=f"Dear {incident.reported_by.username},\n\n{custom_message}\n\nYour incident titled '{incident.title}' has been processed. Please check for updates.",
-                        from_email=from_email,  # Replace with your admin email or environment variable
+                        from_email=from_email, 
                         recipient_list=[incident.reported_by.email],
                     )
                     incident.status = 'Rezolvat'
@@ -138,9 +138,9 @@ def review_incident(request, incident_id):
                     messages.error(request, "There was an error sending the email. Please try again.")
                     return render(request, 'review_incident.html', {'incident': incident, 'form': form})
                 
-                # Add a success message to display after the redirect
+ 
                 messages.success(request, "Răspunsul a fost trimis cu succes!")
-                return HttpResponseRedirect(request.path)  # Redirect back to the same page
+                return HttpResponseRedirect(request.path)
             else:
                 messages.error(request, "No email address found for the user.")
                 return render(request, 'review_incident.html', {'incident': incident, 'form': form})
@@ -150,16 +150,16 @@ def review_incident(request, incident_id):
 def incident_search(request):
     query = request.GET.get('query', '')
     
-    # Attempt to find a matching incident type
+
     guideline = IncidentGuideline.objects.filter(
         incident_type__icontains=query
     ).first()  # Get the first match
     
     if guideline:
-        # Redirect to the guideline detail page for the matched incident_type
+
         return redirect('guideline_detail', incident_type=guideline.incident_type)
     
-    # If no match is found, redirect to the index or another page
+
     return redirect('index')
 
 def recommendation_list(request):
@@ -169,8 +169,6 @@ def recommendation_list(request):
 
 @login_required
 def user_incidents(request):
-    # Get all incidents created by the logged-in user
     incidents = Incident.objects.filter(reported_by=request.user)
     
-    # Render the template with the list of incidents
     return render(request, 'user_incidents.html', {'incidents': incidents})
